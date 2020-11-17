2a92f4f24483ce9f9f8ca8dc373b66e3397ef9d1
Khadija <kabdelouali@enseirb-matmeca.fr>
2020-11-17 11:07:16 +0100

    v1.1.0

--- index.js
+++ index.js
@@ -4,8 +4,20 @@ let bodyParser = require("body-parser");
 let app = express();
 
 app.get("/id", (req, res) => {
-    console.log("Requête GET reçu !!!");
-    return {id: "carpaccio-khadija-amira"}
+    res.status(200).json({
+        "id": "carpaccio-khadija-amira"
+    });
 });
 
-app.listen(3000, () => {console.log("Waiting for requests....");})
+app.post("/bill", (req, res) => {
+    let prices = req.body.prices;
+    let quantities = req.body.quantities;
+    let total = 0;
+    prices.forEach((price, index) => {
+        total += price * quantities[index];
+    });
+    res.status(200).json({
+        "total": total
+    })
+})
+app.listen(3002, () => {console.log("Waiting for requests....");})
