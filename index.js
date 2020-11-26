4a8d2bf923fab4c8cb825d9a4cf70ffd0b036f46
Khadija <kabdelouali@enseirb-matmeca.fr>
2020-11-26 11:00:47 +0100

    vérifications

--- index.js
+++ index.js
@@ -12,12 +12,24 @@ app.get("/id", (req, res) => {
 app.post("/bill", (req, res) => {
     let prices = req.body.prices;
     let quantities = req.body.quantities;
-    let total = 0;
-    prices.forEach((price, index) => {
-        total += price * quantities[index];
-    });
-    res.status(200).json({
-        "total": total
-    })
+    if (prices.length === 0 || quantities.length === 0 || prices.length !== quantities.length) {
+        res.json({
+            "error": "error"
+        })
+    } else {
+        let total = 0;
+        prices.forEach((price, index) => {
+            if (price < 0) {
+                res.json({
+                    "error": "prices should be > 0"
+                });
+            } else {
+                total += price * quantities[index];
+            }
+        });
+        res.status(200).json({
+            "total": total
+        })
+    }
 })
-app.listen(3002, () => {console.log("Waiting for requests....");})
+app.listen(3002, () => { console.log("Waiting for requests...."); })
